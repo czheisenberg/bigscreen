bf1a74c199df9f48f5334700061ff679f671f19f
czheisenberg <czheisenberg@qq.com>
2024-07-16 12:52:15 +0800

    大屏整体布局

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,9 +3,43 @@
     <!-- 全屏容器 http://datav.jiaminghi.com/guide/ -->
     <dv-full-screen-container>
       <!-- 边框 -->
-      <dv-border-box-11 title="dv-border-box-11">
+      <!-- <dv-border-box-11 title="Data Center"> -->
         
-      </dv-border-box-11>
+        <!-- <dv-border-box-2 style="width: 300px; height: 200px;">dv-border-box-2</dv-border-box-2>
+      
+        <dv-border-box-2 style="width: 300px; height: 200px;">dv-border-box-2</dv-border-box-2>
+      
+        <dv-border-box-2 style="width: 300px; height: 200px;">dv-border-box-2</dv-border-box-2> -->
+        <div class="module-box">
+          <div style="flex:0 1 50%">
+            <dv-border-box-10 style="width: 100%; height: 200px;">dv-border-box-2</dv-border-box-10>
+            <dv-border-box-10 style="width: 100%; height: 200px;">dv-border-box-22</dv-border-box-10>
+            <dv-border-box-10 style="width: 100%; height: 200px;">dv-border-box-22</dv-border-box-10>
+          </div>
+          <div style="flex:0 1 50%">
+            <dv-border-box-10 style="width: 100%; height: 600px;">dv-border-box-2</dv-border-box-10>
+          </div>
+          <div style="flex:0 1 50%">
+            <dv-border-box-10 style="width: 100%; height: 300px;">dv-border-box-2</dv-border-box-10>
+            <dv-border-box-10 style="width: 100%; height: 300px;">dv-border-box-2</dv-border-box-10>
+          </div>
+        </div>
+
+        <div class="module-box">
+          <div style="flex:0 1 50%">
+            <dv-border-box-10 style="width: 100%; height: 200px;">dv-border-box-2</dv-border-box-10>
+          </div>
+          <div style="flex:0 1 25%">
+            <dv-border-box-10 style="width: 100%; height: 200px;">dv-border-box-2</dv-border-box-10>
+          </div>
+          <div style="flex:0 1 25%">
+            <dv-border-box-10 style="width: 100%; height: 200px;">dv-border-box-2</dv-border-box-10>
+          </div>
+          
+        </div>
+
+      <!-- </dv-border-box-11> -->
+      
     </dv-full-screen-container>
   </div>
 </template>
