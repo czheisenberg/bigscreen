5a2af05fb47eb7fe8e0a06dca907bc56eb4bcf61
czheisenberg <czheisenberg@qq.com>
2024-07-16 16:39:01 +0800

    feat:props传递数据

--- src/views/TheCapsuleChart.vue
+++ src/views/TheCapsuleChart.vue
@@ -4,31 +4,48 @@
 </template>
 <script>
 export default{
+    props:{
+        // 数据
+        dataChart:{
+            type: Array,
+            default:()=>[
+                {
+                    name: '南阳',
+                    value: 167
+                },
+                {
+                    name: '周口',
+                    value: 67
+                },
+                {
+                    name: '漯河',
+                    value: 123
+                },
+                {
+                    name: '郑州',
+                    value: 55
+                },
+                {
+                    name: '西峡',
+                    value: 98
+                }
+            ]
+        },
+        // 演示
+        colorsChart:{
+            type: Array,
+            default:()=>[
+                
+            ]
+        }
+    },
     data(){
         return{
             config:{
-            data:[
-            {
-                name: '南阳',
-                value: 167
-            },
-            {
-                name: '周口',
-                value: 67
-            },
-            {
-                name: '漯河',
-                value: 123
-            },
-            {
-                name: '郑州',
-                value: 55
-            },
-            {
-                name: '西峡',
-                value: 98
-            }
-            ]
+            data: this.dataChart,
+            colors: this.colorsChart,
+            unit: '单位',
+            showValue: true
         },
         }
     }
--- src/views/TheContent.vue
+++ src/views/TheContent.vue
@@ -3,7 +3,7 @@
     <!-- 左 -->
     <div style="flex:0 1 25%">
       <dv-border-box-13 style="width: 100%; height: 300px;">
-        <TheCapsuleChart/>
+        <TheCapsuleChart :dataChart="dataChart" :colorsChart="colorsChart"/>
       </dv-border-box-13>
       <dv-border-box-10 style="width: 100%; height: 300px;">
         <TheWaterLevelPond/>
@@ -41,7 +41,20 @@ export default{
     TheWaterLevelPond,
     TheConicalColumnChart,
     TheFlylineChart
-}
+},
+  data(){
+    return{
+      dataChart:[
+        {name:"标题1", value:100},
+        {name:"标题2", value:20},
+        {name:"标题3", value:70},
+        {name:"标题4", value:40},
+        {name:"标题5", value:44},
+        {name:"标题6", value:67},
+        {name:"标题7", value:99}, 
+      ]
+    }
+  }
 }
 
 </script>
